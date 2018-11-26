4262ea382fc63888882925979f1df8cbfe3f4d9a
Will Smith <will@fundapps.co>
2018-11-26 16:46:33 +0000

    Initial commit

---
+++ FundApps.Delivery.Tests/TestTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace FundApps.Delivery.Tests
+{
+    public class TestTests
+    {
+        [Fact]
+        public void ShouldFail()
+        {
+            Assert.False(true);
+        }
+    }
+}
