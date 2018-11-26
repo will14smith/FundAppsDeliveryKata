f7a882ae7136803d55b4c932f0e7f4ea2b66e021
Will Smith <will@fundapps.co>
2018-11-26 17:11:56 +0000

    Implement speedy delivery

--- FundApps.Delivery.Tests/ParcelOrdererTests.cs
+++ FundApps.Delivery.Tests/ParcelOrdererTests.cs
@@ -4,34 +4,34 @@ namespace FundApps.Delivery.Tests
 {
     public class ParcelOrdererTests
     {
+        private static readonly ParcelInput[] Inputs = {
+            new ParcelInput(10, 10, 10),
+            new ParcelInput(50, 10, 10),
+            new ParcelInput(100, 10, 10),
+        };
+
         [Fact]
         public void ShouldProcessAllParcelsInTheOrder()
         {
-            var inputs = new[]
-            {
-                new ParcelInput(10, 10, 10),
-                new ParcelInput(50, 10, 10),
-                new ParcelInput(100, 10, 10),
-            };
+            var result = new ParcelOrderer(new ParcelPicker(ParcelTestData.ParcelTypes)).Order(Inputs);
 
-            var result = new ParcelOrderer(new ParcelPicker(ParcelTestData.ParcelTypes)).Order(inputs);
-
-            Assert.Equal(inputs.Length, result.Parcels.Count);
+            Assert.Equal(Inputs.Length, result.Parcels.Count);
         }
 
         [Fact]
         public void ParcelOrderTotalShouldBeCorrect()
         {
-            var inputs = new[]
-            {
-                new ParcelInput(10, 10, 10),
-                new ParcelInput(50, 10, 10),
-                new ParcelInput(100, 10, 10),
-            };
-
-            var result = new ParcelOrderer(new ParcelPicker(ParcelTestData.ParcelTypes)).Order(inputs);
+            var result = new ParcelOrderer(new ParcelPicker(ParcelTestData.ParcelTypes)).Order(Inputs);
 
             Assert.Equal(48, result.TotalPrice);
+        }
+
+        [Fact]
+        public void SpeedOrderShouldDoubleTheTotal()
+        {
+            var result = new ParcelOrderer(new ParcelPicker(ParcelTestData.ParcelTypes)).Order(Inputs, true);
+
+            Assert.Equal(48*2, result.TotalPrice);
         }
     }
 }
--- FundApps.Delivery/ParcelOrderer.cs
+++ FundApps.Delivery/ParcelOrderer.cs
@@ -13,23 +13,26 @@ namespace FundApps.Delivery
             _picker = picker;
         }
 
-        public ParcelOrder Order(IReadOnlyCollection<ParcelInput> inputs)
+        public ParcelOrder Order(IReadOnlyCollection<ParcelInput> inputs, bool isSpeedy = false)
         {
             var parcels = inputs.Select(x => _picker.Pick(x)).ToList();
 
-            return new ParcelOrder(parcels);
+            return new ParcelOrder(parcels, isSpeedy);
         }
     }
 
     public class ParcelOrder
     {
-        public ParcelOrder(IReadOnlyCollection<ParcelSpecification> parcels)
+        private readonly bool _isSpeedy;
+
+        public ParcelOrder(IReadOnlyCollection<ParcelSpecification> parcels, bool isSpeedy)
         {
+            _isSpeedy = isSpeedy;
             Parcels = parcels;
         }
 
         public IReadOnlyCollection<ParcelSpecification> Parcels { get; }
 
-        public decimal TotalPrice => Parcels.Sum(x => x.Price);
+        public decimal TotalPrice => Parcels.Sum(x => x.Price) * (_isSpeedy ? 2 : 1);
     }
 }
