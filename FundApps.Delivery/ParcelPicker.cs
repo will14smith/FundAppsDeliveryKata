eb81aba936f3a0f617305a22c11d6ffe02315ebb
Will Smith <will@fundapps.co>
2018-11-26 16:51:19 +0000

    Created ParcelPicker skeleton

---
+++ FundApps.Delivery/ParcelPicker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundApps.Delivery
+{
+    public class ParcelPicker
+    {
+        private readonly IReadOnlyCollection<ParcelSpecification> _parcelTypes;
+
+        public ParcelPicker(IReadOnlyCollection<ParcelSpecification> parcelTypes)
+        {
+            _parcelTypes = parcelTypes;
+        }
+
+        public ParcelSpecification Pick(ParcelPickerInput input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FundApps.Delivery/ParcelPickerInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundApps.Delivery
+{
+    public class ParcelPickerInput
+    {
+        public ParcelPickerInput(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+        public int Z { get; }
+    }
+}
