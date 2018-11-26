4053dcc8292e10abfa525e0b5c89d14a1b8a33b3
Will Smith <will@fundapps.co>
2018-11-26 16:47:37 +0000

    Create model for package

---
+++ FundApps.Delivery/ParcelSpecification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundApps.Delivery
+{
+    public class ParcelSpecification
+    {
+        public ParcelSpecification(string name, int maxDimension, decimal price)
+        {
+            Name = name;
+            MaxDimension = maxDimension;
+            Price = price;
+        }
+
+        public string Name { get; }
+        public int MaxDimension { get; }
+        public decimal Price { get; }
+    }
+}
