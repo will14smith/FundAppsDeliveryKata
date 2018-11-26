4e9db8d4ccfd1c622a09a2458603ceea0f582cc8
Will Smith <will@fundapps.co>
2018-11-26 16:49:23 +0000

    Create ParcelTestData

---
+++ FundApps.Delivery.Tests/ParcelTestData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundApps.Delivery.Tests
+{
+    public static class ParcelTestData
+    {
+        public static IReadOnlyCollection<ParcelSpecification> ParcelTypes = new List<ParcelSpecification>
+        {
+            new ParcelSpecification("Small", 10, 3),
+            new ParcelSpecification("Medium", 50, 8),
+            new ParcelSpecification("Large", 100, 15),
+            new ParcelSpecification("Extra Large", int.MaxValue, 25),
+        };
+    }
+}
